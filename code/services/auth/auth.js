64c704d369b5de1cfa127b52df8a9821fd89df45
clearbladeplatform <platform@clearblade.com>
2017-09-11 18:25:50 -0500

    created by clearbladeplatform

---
+++ code/services/auth/auth.js
@@ -0,0 +1,20 @@
+function auth(req, resp){
+    
+    if(devTokenExists){
+        // logThemInLocally
+    }
+    
+    // take in username, password, domain (zilker.clearblade.com)
+    // REST request 
+    // curl -X POST "https://platform.clearblade.com/api/v/1/user/auth" 
+    // -H "accept: application/json" -H "ClearBlade-SystemKey: a" 
+    //-H "ClearBlade-SystemSecret: a" -H "content-type: application/json" 
+    // -d "{ \"email\": \"cbman@clearblade.com\", \"password\": \"cl34rbl4d3\"}"
+    
+    if(exists){
+        // registerUser ...
+        // user account cache
+    }
+    
+    // return true, false    
+}
